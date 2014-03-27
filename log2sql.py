20a35a086f828b7633572653e68aeca751f0e137
Morketh <admmorketh@gmail.com>
2014-03-27 15:00:55 -0700

    fix missing # and changed mode to a+x

--- log2sql.py
+++ log2sql.py
@@ -1,4 +1,4 @@
-!/usr/bin/python
+#!/usr/bin/python
 ##################################################################################################
 #
 # Copyright (C) 2013 Collective Industries code provided by Andrew Malone 
