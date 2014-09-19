6b684b64ba252a1daff239ae8e978c165312aee7
Drew Malone <admmorketh@gmail.com>
2014-09-19 17:16:12 -0500

    adds GitPython API fixes issue #6

--- mangos_install_2.py
+++ mangos_install_2.py
@@ -35,12 +35,14 @@ import os.path
 import glob
 import CI_COLORS # COLOR CODES
 import datetime
+from git import * #for our git tools and bindings
 
 ## SCRIPT GLOBALS ##
 _DEBUG_ = False
-SERV_CODE = '/home/' + SYS_USR + '/SOURCE/mangos3_ci_code' #will be used to clone all the code and compile the software (can be removed after the install)
+SERV_HOME = '/home/' + SYS_USR
+CODE_BASE = SERV_HOME + '/SOURCE' #will be used to clone all the code and compile the software (can be removed after the install)
 SQL_USR_INST = 'mangos-ci-usr.sql'
-_LOC_SQL_UPDATES_ = SERV_CODE + '/server/sql/updates/'
+## _LOC_SQL_UPDATES_ = SERV_CODE + '/server/sql/updates/'
 
 ##################################### Function Definitions #####################################
 
@@ -62,7 +64,7 @@ def cur_pos(x_pos,y_pos,MSG,color):
 # CI MANGOS LOGO HERE
 # Idea by Levi Modl
 # adapted to work with Python by Andrew Malone
-# color implimented 9-15-2014 sugestted by William Baggett 
+# color implimented 9-15-2014 sugested by William Baggett 
 def logo():                                                                              ## PRINTS OUT WHITE BOARDER ##
 	print "\x1b[0;92;40m                                                              \x1b[0;32;47m  \x1b[0m" #bright green on black no formatting
 	print "\x1b[0;92;40m CCCCC       IIIIIIIII                                        \x1b[0;32;47m  \x1b[0m"
@@ -87,7 +89,7 @@ def logo():                                                                     
 	print "\x1b[0;91;44mMM   MM AA  AAA NN   NN  GGGGGG  OOOO   SSSSS                 \x1b[0;32;47m  \x1b[0m"
 	print "\x1b[0;91;44m        AA  AAA                                               \x1b[0;32;47m  \x1b[0m"
 	print "\x1b[0;91;44m        AAAAAA                                                \x1b[0;32;47m  \x1b[0m"
-	print "\x1b[0m                            http://www.getmangos.co.uk/       \x1b[0;32;47m  \x1b[0m"
+	print "\x1b[0;91;44m                           http://www.getmangos.co.uk/        \x1b[0;32;47m  \x1b[0m"
 	print ""
 	(width, height) = getTerminalSize()
 	print "\x1b[0;32;47m" #White block border #
@@ -102,7 +104,6 @@ def reset_scrn():
         logo()
 	prt_dict(INSTALLER_SETTINGS,10)
 	
-
 ## Returns a formatted Time Stamp ##
 def TimeStamp():
 	return datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
@@ -175,7 +176,12 @@ INSTALLER_SETTINGS = {	"realm_name": 		"",
 			## DATABASE USERS AND PASSWORDS ##
 			"mysql_root_ci_usr":	"root",
 			"mysql_root_ci_pass":	"",
-			
+			## GIT CODE LOCATIONS ##
+			"GIT_REPO_CI_SERVER":	CODE_BASE+"/server",
+			"GIT_REPO_CI_DBS":	CODE_BASE+"/database",
+			"GIT_REPO_CI_SD2":	CODE_BASE+"/server/src/bindings/ScriptDev2", ## SCRIPT DEV LIBARAY DESTINATION
+			"GIT_REPO_CI_TOOLS":	CODE_BASE+"/tools",
+			"GIT_REPO_CI_WEB":	CODE_BASE+"/web",			
 			## FILE STRUCTURE ##
 			"MANGOS_LOGS_DIR": 	"logs",# Path is relitave to the INSTALL_DIR
 			"MANGOS_DATA_DIR":	"data" # Path is relitave to the INSTALL_DIR
@@ -186,7 +192,7 @@ INSTALLER_SETTINGS = {	"realm_name": 		"",
 def prt_dict(stuff,start):
 	"""prints out key value pairs on seprate lines"""
 	(width, height) = getTerminalSize()
-	x_pos = width - 50
+	x_pos = width - 70
 	y_pos = start
 	print "\x1b[4;32;40m"
 	print "\033[%s;%sH%s" % (start,x_pos,"-=/\=-    MaNGOS Install Options    -=/\=-")
@@ -195,6 +201,22 @@ def prt_dict(stuff,start):
 		y_pos += 1
 		print "\033[%s;%sH     %s" % (y_pos,x_pos,k)
 		print "\033[%s;%sH%s" % (y_pos,x_pos+26,v)
+def UserName():
+	return getpass.getuser()
+
+## Clean a full directory path ##
+def clean_dir(path):
+	for file in os.listdir(path):
+		file_path = os.path.join(path, file)
+		try:
+			if os.path.isfile(file_path):
+				os.unlink(file_path)# Clean out files
+				cur_pos(1,26,TimeStamp()+" Unlinking: "+file_path)
+			elif os.path.isdir(file_path):
+				shutil.rmtree(file_path)# delete folders recursively
+				cur_pos(1,26,TimeStamp()+" Removing Directory Tree: "+file_path)
+		except Exception, e:
+			print e
 
 ## START OF MAIN PROGRAM ##
 def main():
@@ -240,9 +262,32 @@ def main():
 	reset_scrn()
         cur_pos(1,26,"\x1b[1;31;40m"+INSTALLER_SETTINGS["m_sys_usr"]+"\x1b[4;32;40m System Password: [\x1b[1;31;40m"+INSTALLER_SETTINGS["m_sys_pass"]+"\x1b[4;32;40m]","4;32;40")
         set_option("m_sys_pass",raw_input("PASSWORD: "),INSTALLER_SETTINGS["m_sys_pass"])
-	
-	## Configuration Files ##
 
+
+	## BUILD PATH ##
+	subprocess.call(shlex.split('sudo rm -Rf '+SERV_HOME))
+	subprocess.call(shlex.split('sudo groupadd --system '+INSTALLER_SETTINGS["m_sys_usr"]))
+	subprocess.call(shlex.split('sudo mkdir -p '+CODE_BASE))
+	subprocess.call(shlex.split('sudo chown -R '+UserName()+':'+INSTALLER_SETTINGS["m_sys_usr"]+' '+CODE_BASE))## set perms for user to clone ##
+	## Initilize Repository ##
+	reset_scrn()
+	cur_pos(1,28,"CLONING REPOSITORY TO "+INSTALLER_SETTINGS["GIT_REPO_CI_SERVER"],"1;31;40")
+	git_server_handle = Repo.clone_from("https://github.com/CollectiveIndustries/server.git",INSTALLER_SETTINGS["GIT_REPO_CI_SERVER"])
+	reset_scrn()
+	cur_pos(1,28,"CLONING REPOSITORY TO "+INSTALLER_SETTINGS["GIT_REPO_CI_DBS"],"1;31;40")
+	git_database_handle = Repo.clone_from("https://github.com/CollectiveIndustries/Mangos_world_database.git",INSTALLER_SETTINGS["GIT_REPO_CI_DBS"])
+	reset_scrn()
+	cur_pos(1,28,"CLONING REPOSITORY TO "+INSTALLER_SETTINGS["GIT_REPO_CI_SD2"],"1;31;40")
+	git_maps_handle = Repo.clone_from("https://github.com/CollectiveIndustries/scripts.git",INSTALLER_SETTINGS["GIT_REPO_CI_SD2"])
+	reset_scrn()
+	cur_pos(1,28,"CLONING REPOSITORY TO "+INSTALLER_SETTINGS["GIT_REPO_CI_WEB"],"1;31;40")
+	git_web_handle = Repo.clone_from("https://github.com/CollectiveIndustries/mangos-enhanced.git",INSTALLER_SETTINGS["GIT_REPO_CI_WEB"])
+
+	## change owner of directories ##
+	subprocess.call(shlex.split('sudo chown -R '+UserName()+':'+INSTALLER_SETTINGS["m_sys_usr"]+' '+CODE_BASE))## set the owner of the directory so we can leave ROOT
+		
+	## Configuration Files ##
+	
 
 	## DATABASE QUESTION ##
 	
